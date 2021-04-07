c14ad2da349c886949983e9fefb009ca78b6153e
hamak <hamakaze181and189amarube@gmail.com>
2021-04-07 23:39:15 +0900

    モデルの重み固定後にサマリ表示するように変更

--- model_preprocessor/weight/fix.py
+++ model_preprocessor/weight/fix.py
@@ -4,7 +4,10 @@ from typing import Callable
 
 def fix_layer_weight(layer_depth: int) -> Callable[[keras.engine.training.Model], keras.engine.training.Model]:
     def fix_weight(target_model: keras.engine.training.Model):
+        print("fix weight")
         for layer in target_model.layers[:layer_depth]:
             layer.trainable = False
+        print("fixed model summary")
+        target_model.summary()
         return target_model
     return fix_weight
