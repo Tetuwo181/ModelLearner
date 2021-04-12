16d94fecc01e0178518bba76835a8cad37a437e1
hamak <hamakaze181and189amarube@gmail.com>
2021-04-12 22:48:17 +0900

    tflite変換処理追加

--- network_model/converter/lite.py
+++ network_model/converter/lite.py
@@ -2,14 +2,11 @@ import numpy as np
 import tensorflow as tf
 
 
-def convert_2_tflite(input_model_path: str,
-                     input_name: str,
-                     output_path: str,
-                     output_node_name: str) -> None:
-    converter = tf.lite.TFLiteConverter.from_frozen_graph(input_model_path,
-                                                          [input_name],
-                                                          [output_node_name])
-    tflite_quant_model = converter.convert()
+def keras_2_tflite(input_model_path: str, output_path: str) -> None:
+    keras_model = tf.keras.models.load_model(input_model_path)
+    converter = tf.lite.TFLiteConverter.from_keras_model(keras_model)
+    print(converter)
+    tflite_model = converter.convert()
     with open(output_path, 'wb') as o_:
-        o_.write(tflite_quant_model)
+        o_.write(tflite_model)
 
