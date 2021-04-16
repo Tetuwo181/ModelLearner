b4ce2be1402a9e4bf5000b8280b352e1dedb1741
hamak <hamakaze181and189amarube@gmail.com>
2021-04-17 01:36:18 +0900

    float16で量子化する箇所修正

--- network_model/converter/lite.py
+++ network_model/converter/lite.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import os
+from typing import Union, List
 
 
 def build_base_converter(input_model_path: str):
@@ -9,6 +10,7 @@ def build_base_converter(input_model_path: str):
 
 def convert_tflite_default(input_model_path: str):
     converter = build_base_converter(input_model_path)
+    converter.optimizations = [tf.lite.Optimize.DEFAULT]
     return converter.convert()
 
 
@@ -22,7 +24,7 @@ def convert_tflite_int8_model(input_model_path: str):
 
 def convert_tflite_float16_model(input_model_path: str):
     converter = build_base_converter(input_model_path)
-    converter.target_spec.supported_ops = [tf.lite.constants.FLOAT16]
+    converter.target_spec.supported_ops = [tf.compat.v1.lite.constants.FLOAT16]
 
     return converter.convert()
 
@@ -42,7 +44,7 @@ def keras_2_tflite(input_model_path: str, output_path: str, mode: str = "default
         o_.write(tflite_model)
 
 
-def build_after_record_tflite_recorder(input_model_path: str, mode: str = "default"):
+def build_after_record_tflite_recorder(input_model_path: str, mode: Union[str, List[str]] = "default"):
     """
     学習を行った後モデルを返還するために必要なラッパー
     :param input_model_path:
@@ -51,8 +53,13 @@ def build_after_record_tflite_recorder(input_model_path: str, mode: str = "defau
     """
     output_dir_path = os.path.dirname(input_model_path)
     file_name_base = os.path.splitext(os.path.basename(input_model_path))[0]
-    output_path = os.path.join(output_dir_path, file_name_base + "_" + mode + ".tflite")
 
     def build_tflite():
-        return keras_2_tflite(input_model_path, output_path, mode)
+        if type(mode) is str and mode != "all":
+            output_path = os.path.join(output_dir_path, file_name_base + "_" + mode + ".tflite")
+            return keras_2_tflite(input_model_path, output_path, mode)
+        write_modes = ["int8", "float16", "default"] if mode == "all" else mode
+        for write_mode in write_modes:
+            output_path = os.path.join(output_dir_path, file_name_base + "_" + write_mode + ".tflite")
+            keras_2_tflite(input_model_path, output_path, write_mode)
     return build_tflite
