a87d3152c914d2ea6176572c82225d4747290eb6
hamak <hamakaze181and189amarube@gmail.com>
2021-04-29 00:48:47 +0900

    tfliteモデル読み込みを重みとモデルファイルが分かれている場合にも対応できるように改修

--- network_model/converter/lite.py
+++ network_model/converter/lite.py
@@ -1,29 +1,30 @@
 import tensorflow as tf
+from network_model.model_base import tempload
 import os
-from typing import Union, List
+from typing import Union, List, Tuple
 
 
-def build_base_converter(input_model_path: str):
-    keras_model = tf.keras.models.load_model(input_model_path)
+def build_base_converter(input_model_paths: Union[str, Tuple[str, str]]):
+    keras_model = tempload.builder(input_model_paths)
     return tf.lite.TFLiteConverter.from_keras_model(keras_model)
 
 
-def convert_tflite_default(input_model_path: str):
-    converter = build_base_converter(input_model_path)
+def convert_tflite_default(input_model_paths: Union[str, Tuple[str, str]]):
+    converter = build_base_converter(input_model_paths)
     converter.optimizations = [tf.lite.Optimize.DEFAULT]
     return converter.convert()
 
 
-def convert_tflite_int8_model(input_model_path: str):
-    converter = build_base_converter(input_model_path)
+def convert_tflite_int8_model(input_model_paths: Union[str, Tuple[str, str]]):
+    converter = build_base_converter(input_model_paths)
     converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
     converter.inference_input_type = tf.int8  # or tf.uint8
     converter.inference_output_type = tf.int8  # or tf.uint8
     return converter.convert()
 
 
-def convert_tflite_float16_model(input_model_path: str):
-    converter = build_base_converter(input_model_path)
+def convert_tflite_float16_model(input_model_paths: Union[str, Tuple[str, str]]):
+    converter = build_base_converter(input_model_paths)
     converter.optimizations = [tf.lite.Optimize.DEFAULT]
     converter.target_spec.supported_ops = [tf.compat.v1.lite.constants.FLOAT16]
 
@@ -38,29 +39,30 @@ def get_tflite_converter(mode: str = "default"):
     return convert_tflite_default
 
 
-def keras_2_tflite(input_model_path: str, output_path: str, mode: str = "default") -> None:
+def keras_2_tflite(input_model_path: Union[str, Tuple[str, str]], output_path: str, mode: str = "default") -> None:
     converter = get_tflite_converter(mode)
     tflite_model = converter(input_model_path)
     with open(output_path, 'wb') as o_:
         o_.write(tflite_model)
 
 
-def build_after_record_tflite_recorder(input_model_path: str, mode: Union[str, List[str]] = "default"):
+def build_after_record_tflite_recorder(input_model_paths: Union[str, Tuple[str, str]], mode: Union[str, List[str]] = "default"):
     """
     学習を行った後モデルを返還するために必要なラッパー
-    :param input_model_path:
+    :param input_model_paths:
     :param mode:
     :return:
     """
-    output_dir_path = os.path.dirname(input_model_path)
-    file_name_base = os.path.splitext(os.path.basename(input_model_path))[0]
+    output_base = input_model_paths[0] if type(input_model_paths) is tuple else input_model_paths
+    output_dir_path = os.path.dirname(output_base)
+    file_name_base = os.path.splitext(os.path.basename(output_base))[0]
 
     def build_tflite():
         if type(mode) is str and mode != "all":
             output_path = os.path.join(output_dir_path, file_name_base + "_" + mode + ".tflite")
-            return keras_2_tflite(input_model_path, output_path, mode)
+            return keras_2_tflite(input_model_paths, output_path, mode)
         write_modes = ["int8", "float16", "default"] if mode == "all" else mode
         for write_mode in write_modes:
             output_path = os.path.join(output_dir_path, file_name_base + "_" + write_mode + ".tflite")
-            keras_2_tflite(input_model_path, output_path, write_mode)
+            keras_2_tflite(input_model_paths, output_path, write_mode)
     return build_tflite
