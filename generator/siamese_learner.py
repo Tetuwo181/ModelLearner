b179b86e4c4775e83af6202ef252968679dac18b
hamak <hamakaze181and189amarube@gmail.com>
2021-08-15 00:00:49 +0900

    マージンの箇所の処理が効いていなかったので修正

--- generator/siamese_learner.py
+++ generator/siamese_learner.py
@@ -7,7 +7,7 @@ def build_batch_for_siameselearner(data_batch, teachers, margin=1):
     shuffled_dataset = np.random.permutation(data_teacher_pair)
     other_batch = np.array([data[0] for data in shuffled_dataset])
     other_teachers = [data[1] for data in shuffled_dataset]
-    shame_labels = [build_shame_label(base_label, other_label) for (base_label, other_label)
+    shame_labels = [build_shame_label(base_label, other_label, margin) for (base_label, other_label)
                     in zip(teachers, other_teachers)]
     return [data_batch, other_batch], np.array(shame_labels, dtype="f4")
 
